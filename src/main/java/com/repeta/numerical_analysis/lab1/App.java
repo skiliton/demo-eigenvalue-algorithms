6b9188f137de33a04fe000d65d8f5b6261e9d738
Ivan Repeta <ivan.repeta.work@gmail.com>
2020-09-15 15:16:04 +0300

    Initial commit

---
+++ src/main/java/com/repeta/numerical_analysis/lab1/App.java
@@ -0,0 +1,13 @@
+package com.repeta.numerical_analysis.lab1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
