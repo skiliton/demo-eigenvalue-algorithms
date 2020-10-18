c516fa59862594e5eeb2cf67b874eaf020c67ff0
Ivan Repeta <ivan.repeta.work@gmail.com>
2020-10-18 16:52:47 +0300

    Add app.properties file and configuration lib
    In order to solve problem of duplicating default values
    Apache Commons Configuration library was added. ConfigurationSingleton
    was created to make shure that only one configuration access point
    exist across program

--- src/main/java/com/repeta/numerical_analysis/lab1/eigenvalue/EigenvalueAlgorithm.java
+++ src/main/java/com/repeta/numerical_analysis/lab1/eigenvalue/EigenvalueAlgorithm.java
@@ -1,13 +1,16 @@
 package com.repeta.numerical_analysis.lab1.eigenvalue;
 
+import com.repeta.numerical_analysis.lab1.ConfigurationSingleton;
 import org.ejml.simple.SimpleMatrix;
 
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
 
+import static com.repeta.numerical_analysis.lab1.ConfigurationSingleton.*;
+
 public abstract class EigenvalueAlgorithm implements Function<SimpleMatrix,Map<Double,List<SimpleMatrix>>> {
-    protected double eps = 0.0001;
+    protected double eps = getConfiguration().getDouble("algorithm.default-eps");
 
     public EigenvalueAlgorithm setEps(double eps) {
         this.eps = eps;
